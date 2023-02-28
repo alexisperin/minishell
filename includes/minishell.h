95a9265b6d14a3fd76253b90b4761e55abe3e015
aperin <aperin@student.s19.be>
2023-02-28 11:55:10 +0100

    Push

--- includes/minishell.h
+++ includes/minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aburnott <aburnott@student.s19.be>         +#+  +:+       +#+        */
+/*   By: aperin <aperin@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/25 14:36:16 by aperin            #+#    #+#             */
-/*   Updated: 2023/02/26 11:20:54 by aburnott         ###   ########.fr       */
+/*   Updated: 2023/02/28 10:39:12 by aperin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,14 +28,14 @@
 # include <sys/stat.h>
 # include <fcntl.h>
 
-# define NRM  "\x1B[0m"
-# define RED  "\x1B[31m"
-# define GRN  "\x1B[32m"
-# define YEL  "\x1B[33m"
-# define BLU  "\x1B[34m"
-# define MAG  "\x1B[35m"
-# define CYN  "\x1B[36m"
-# define WHT  "\x1B[37m"
+# define NRM "\x1B[0m"
+# define RED "\x1B[31m"
+# define GRN "\x1B[32m"
+# define YEL "\x1B[33m"
+# define BLU "\x1B[34m"
+# define MAG "\x1B[35m"
+# define CYN "\x1B[36m"
+# define WHT "\x1B[37m"
 
 # ifndef PATH_MAX
 #  define PATH_MAX 1024
